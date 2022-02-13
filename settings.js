2083fcfb7d3604b8b616bf600ac846c45c5a6a58
putragans9 <putragans970@gmail.com>
2022-02-13 18:22:57 +0700

    scmd

---
+++ settings.js
@@ -0,0 +1,62 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	alfa: 'https://subscribe-tod.herokuapp.com',
+}
+
+// Free apikey
+global.APIKeys = {
+	'https://api-alphabot.herokuapp.com': 'Alphabot',
+}
+
+// setting 
+global.autoread = true // auto read pesan / message
+global.autorecording = true //status auto merekam ( auto record )
+global.autoketik = false //status auto mengetik (auto typing)
+global.available = false //status online (online)
+
+// Other
+global.botname = "PUTRAXD"
+global.ownername= "PUTRAXD OFC"
+global.myweb ="https://subscribe-tod.herokuapp.com/"
+global.youtube = "https://youtube.com/channel/UCJGX0lPZWVDidVL2pEFDaSw"
+global.github = "https://github.com/putragans9"
+global.ownernomer = "6283842942635"
+global.ownernomerr = "+6283842942635"
+global.thumbnail = "./image/PutraXd.jpg"
+global.donasi = "./image/donasi.jpg"
+global.background_welcome="https://telegra.ph/file/4e3581f3ae06bfa621247.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6283842942635","6283812692567","6283174508962"] //ganti agar fitur owner bisa di gunakan
+global.packname = 'PutraXd' //sticker wm
+global.author = 'PutraXd' //sticker wm
+global.sessionName = 'session'
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
